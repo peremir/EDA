9737482bd58e1bd940fa442b70c00217699221ec
peremir <57814247+peremir@users.noreply.github.com>
2021-11-15 12:59:30 +0100

    Update P50709.cc

--- Use of STL data structures/P50709.cc
+++ Use of STL data structures/P50709.cc
@@ -1,1 +1,90 @@
 
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+	
+	map<int, int> numeros;
+	map<int, int>::iterator it;
+	char instr;
+	
+	while(cin >> instr) {
+	
+		switch(instr) {
+		/* ==================================== */
+		case 'S':
+		
+		int num; 
+		cin >> num;
+		
+		numeros[num]++;
+		
+		break;
+		/* ==================================== */
+		case 'A':
+		
+		if (numeros.empty()) cout << "error!" << endl;
+		else {
+		
+			it = numeros.end();
+			--it;
+			
+			cout << (*it).first << endl;
+		}
+		
+		break;
+		/* ==================================== */
+		case 'R':
+		
+		if (numeros.empty()) cout << "error!" << endl;
+		else {
+			it = numeros.end();
+			--it;
+			
+			if ((*it).second == 1) numeros.erase(it);
+			else --(*it).second;
+		}
+		
+		break;
+		/* ==================================== */
+		case 'I':
+		
+		if (numeros.empty()) cout << "error!" << endl;
+		else {
+			int x; 
+			cin >> x;
+			
+			it = numeros.end();
+			--it;
+			
+			numeros[((*it).first)+x]++;
+			
+			if ((*it).second == 1) numeros.erase(it);
+			else --(*it).second;
+		}
+		
+		break;
+		/* ==================================== */
+		case 'D':
+		
+		if (numeros.empty()) cout << "error!" << endl;
+		else {
+			int y; 
+			cin >> y;
+			
+			it = numeros.end();
+			--it;
+			
+			numeros[((*it).first)-y]++;
+			
+			if ((*it).second == 1) numeros.erase(it);
+			else --(*it).second;
+		}
+		
+		break;
+		/* ==================================== */
+		}
+	}
+}
+
