70a17d539bbf499a98cda53058b626e8cd8439dc
peremir <57814247+peremir@users.noreply.github.com>
2021-11-15 12:54:58 +0100

    Update P84415.cc

--- Use of STL data structures/P84415.cc
+++ Use of STL data structures/P84415.cc
@@ -1,1 +1,52 @@
 
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+    
+    map<string,int> m;
+    
+    string s;
+    while (cin >> s) {
+        
+        if (s == "store") {
+            
+            string word;
+            cin >> word;
+            
+            m[word]++;
+        }
+        else if (s == "delete") {
+            
+            string word;
+            cin >> word;
+            
+            map<string,int>::iterator it = m.find(word);
+            if (it->second == 1) m.erase(it);
+            else it->second--;
+        }
+        else if (s == "minimum?") {
+            
+            if (m.empty()) cout << "indefinite minimum" << endl;
+            else {
+                
+                map<string,int>::iterator it = m.begin();
+                
+                cout << "minimum: " << it->first << ", " << it->second << " time(s)" << endl;
+            }
+        }
+        else if (s == "maximum?") {
+        
+            if (m.empty()) cout << "indefinite maximum" << endl;
+            else {
+                
+                map<string,int>::iterator it = m.end(); --it;
+                
+                cout << "maximum: " << it->first << ", " << it->second << " time(s)" << endl;
+            }
+        }
+    }
+}
+
